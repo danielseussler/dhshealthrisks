7595be482b12eb0987dc60c082860f7b4231032c
danielseussler <daniel.seussler@gmail.com>
2022-12-03 16:22:05 +0100

    Create get_shapefiles.R

---
+++ src/data/get_shapefiles.R
@@ -0,0 +1,63 @@
+# create country shape files from GADM database and DHS survey files
+#
+#
+#
+
+library(here)
+library(purrr)
+library(sf)
+
+madagascar = map(
+  .x = c(0, 1, 2, 3),
+  .f = ~ geodata::gadm(
+    country = "Madagascar",
+    level = .,
+    path = tempdir(),
+    version = "4.1",
+    resolution = 2
+  ) |>
+    st_as_sf() |>
+    st_transform(crs = 4326)
+) |> setNames(c("admin0", "admin1", "admin2", "admin3"))
+
+
+mali = map(
+  .x = c(0, 1, 2),
+  .f = ~ geodata::gadm(
+    country = "Mali",
+    level = .,
+    path = tempdir(),
+    version = "4.1",
+    resolution = 2
+  ) |>
+    st_as_sf() |>
+    st_transform(crs = 4326)
+) |> setNames(c("admin0", "admin1", "admin2"))
+
+ethiopia = map(
+  .x = c(0, 1, 2),
+  .f = ~ geodata::gadm(
+    country = "Ethiopia",
+    level = .,
+    path = tempdir(),
+    version = "4.1",
+    resolution = 2
+  ) |>
+    st_as_sf() |>
+    st_transform(crs = 4326)
+) |> setNames(c("admin0", "admin1", "admin2"))
+
+saveRDS(object = ethiopia, file = here("data", "processed", "ethiopia", "regions.rds"))
+saveRDS(object = madagascar, file = here("data", "processed", "madagascar", "regions.rds"))
+saveRDS(object = mali, file = here("data", "processed", "mali", "regions.rds"))
+
+
+# load dhs data from spatial repository
+ethiopia = read_sf(here("data/raw/sdr_subnational_boundaries_2022-11-26_ethiopia_2019_dhs/sdr_subnational_boundaries.shp"))
+madagascar = read_sf(here("data/raw/sdr_subnational_boundaries_2022-11-26_madagascar_2021_dhs/sdr_subnational_boundaries.shp"))
+mali = read_sf(here("data/raw/sdr_subnational_boundaries_2022-11-26_mali_2021_mis/sdr_subnational_boundaries.shp"))
+
+saveRDS(object = ethiopia, file = here("data", "processed", "ethiopia", "dhsboundaries.rds"))
+saveRDS(object = madagascar, file = here("data", "processed", "madagascar", "dhsboundaries.rds"))
+saveRDS(object = mali, file = here("data", "processed", "mali", "dhsboundaries.rds"))
+
