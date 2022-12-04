9fedf46e4a3b9caf11a3dee6daacd9810182d450
danielseussler <daniel.seussler@gmail.com>
2022-12-04 22:14:07 +0100

    Update plot_simulation.R

--- src/analysis/madagascar/plot_simulation.R
+++ src/analysis/madagascar/plot_simulation.R
@@ -6,10 +6,11 @@
 library(here)
 library(data.table)
 library(ggplot2)
+library(viridis)
 library(Metrics)
 
 theme_set(theme_light())
-load(file = here("results", "88qlclkf.rda"))
+load(file = here("models", "88qlclkf.rda"))
 
 results[, predc := as.integer(pred >= 0.5)]
 results[, method := paste(type, "\n", nfold, "-folds ", nrep, "-rep", sep = "")]
@@ -34,8 +35,9 @@ tmp = melt(metr, id.vars = c("holdout", "method", "cvstrat"), measure.vars = c("
 plt.1 = ggplot(tmp, aes(x = method, y = value, fill = cvstrat)) +
   geom_boxplot(outlier.alpha = 0.3, alpha = 0.6) +
   scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
-  scale_fill_manual(values = viridis(n = 2, alpha = 1, begin = 0.3, end = 0.7)) +
+  scale_fill_manual(values = viridis(n = 2, alpha = 0.9, begin = 0.3, end = 0.7), name = "Stratification") +
   facet_grid(variable ~ holdout, scales = "free") +
+  labs(x = "Method", y = "") +
   theme(legend.position = "bottom")
 
-ggsave(plot = plt.1, filename = "fig_simresults_mdg.png", path = here("figures"), dpi = 600, scale = 0.9, width = 210, height = 297, units = "mm", device = png)
+ggsave(plot = plt.1, filename = "fig_mdg_simresults.png", path = here("results", "figures"), dpi = 600, scale = 0.9, width = 210, height = 297, units = "mm", device = png)
