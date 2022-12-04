58fc3384fbc0578910bad5e8f47171ede8e8dc5c
danielseussler <daniel.seussler@gmail.com>
2022-12-04 14:16:25 +0100

    Update formula.R

--- src/analysis/mali/formula.R
+++ src/analysis/mali/formula.R
@@ -1,4 +1,4 @@
-# model formula
+# model formulas
 #
 #
 #
@@ -9,44 +9,7 @@
 frml.1 = list(
   mu = y ~
     bols(intercept, intercept = FALSE) +
-    bols(urban) +
-    bols(climate) +
-
-    bols(elev) +
-    bols(elev, by = urban) +
-
-    bols(lstday) +
-    bols(lstday, by = urban) +
-
-    bols(lstnight) +
-    bols(lstnight, by = urban) +
-
-    bols(ndvi) +
-    bols(ndvi, by = urban) +
-
-    bols(evi) +
-    bols(evi, by = urban) +
-
-    bols(precip) +
-    bols(precip, by = urban) +
-
-    bols(pop) +
-    bols(pop, by = urban) +
-
-    bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE),
-
-  sigma = y ~
-    bols(intercept, intercept = FALSE) +
-    bols(urban) +
-    bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE)
-)
-
-
-frml.2 = list(
-  mu = y ~
-    bols(intercept, intercept = FALSE) +
+    
     bols(urban) +
     bols(climate) +
 
@@ -67,106 +30,23 @@ frml.2 = list(
 
     bols(precip) +
     bols(precip, by = climate) +
-    bols(precip, by = urban) +
 
     bols(pop) +
     bols(pop, by = climate) +
 
     bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE),
+    bspatial(lon, lat, df = 1L, center = TRUE),
 
   sigma = y ~
     bols(intercept, intercept = FALSE) +
     bols(urban) +
+    
     bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE)
+    bspatial(lon, lat, df = 1L, center = TRUE)
 )
 
 
-frml.3 = list(
-  mu = y ~
-    bols(intercept, intercept = FALSE) +
-    bols(urban) +
-    bols(climate) +
-
-    bols(elev) +
-    bols(elev, by = climate) +
-    bols(elev, by = urban) +
-
-    bols(lstday) +
-    bols(lstday, by = climate) +
-    bols(lstday, by = urban) +
-
-    bols(lstnight) +
-    bols(lstnight, by = climate) +
-    bols(lstnight, by = urban) +
-
-    bols(ndvi) +
-    bols(ndvi, by = climate) +
-    bols(ndvi, by = urban) +
-
-    bols(evi) +
-    bols(evi, by = climate) +
-    bols(evi, by = urban) +
-
-    bols(precip) +
-    bols(precip, by = climate) +
-    bols(precip, by = urban) +
-
-    bols(pop) +
-    bols(pop, by = climate) +
-    bols(pop, by = urban) +
-
-    bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE),
-
-  sigma = y ~
-    bols(intercept, intercept = FALSE) +
-    bols(urban) +
-    bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE)
-)
-
-
-
-frml.b = y ~
-    bols(intercept, intercept = FALSE) +
-    bols(urban) +
-    bols(climate) +
-
-    bols(elev) +
-    bols(elev, by = climate) +
-    bols(elev, by = urban) +
-
-    bols(lstday) +
-    bols(lstday, by = climate) +
-    bols(lstday, by = urban) +
-
-    bols(lstnight) +
-    bols(lstnight, by = climate) +
-    bols(lstnight, by = urban) +
-
-    bols(ndvi) +
-    bols(ndvi, by = climate) +
-    bols(ndvi, by = urban) +
-
-    bols(evi) +
-    bols(evi, by = climate) +
-    bols(evi, by = urban) +
-
-    bols(precip) +
-    bols(precip, by = climate) +
-    bols(precip, by = urban) +
-
-    bols(pop) +
-    bols(pop, by = climate) +
-    bols(pop, by = urban) +
-
-    bols(lon) + bols(lat) + bols(lon, by = lat) +
-    bspatial(lon, lat, df = 1, center = TRUE)
-
-
-frml.bb = list(
+frml.tree = list(
   mu = y ~ intercept + urban + climate + elev + lstday + lstnight + ndvi + evi + precip + pop + lon + lat,
   sigma = y ~ intercept + urban + lon + lat
 )
