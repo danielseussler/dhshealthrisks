2346e99101d262774772206bfc83c4cd09251b19
danielseussler <daniel.seussler@gmail.com>
2022-12-09 16:46:33 +0100

    Update analysis.R

--- src/analysis/mali/analysis.R
+++ src/analysis/mali/analysis.R
@@ -21,7 +21,9 @@ cl = dplyr::mutate(cl, pop = log(pop + 1.0))
 # define outcome
 y = matrix(c(cl$npos, cl$nneg), ncol = 2L)
 
-# run model
+
+# fit distributional boosting model with beta binomial distribution
+# resampling based on survey stratified folds
 mod = gamboostLSS(
   formula = frml.1
   , data = cl
@@ -30,28 +32,24 @@ mod = gamboostLSS(
   , control = boost_control(mstop = 1000L, nu = 0.25, trace = TRUE)
 )
 
-# resampling based on survey stratified folds
 cv = cvrisk(
   object = mod
+  , grid = 1:mstop(mod)
   , folds = cv(weights = model.weights(mod), type = "subsampling", B = 25L, strata = cl$strata)
-  , grid = make.grid(max = 1000L, length.out = 500L, log = TRUE)
 )
 
 mod[mstop(cv)]
+
+plot(mod)
+
 table(selected(mod)$mu)
 table(selected(mod)$sigma)
 
 names(coef(mod, parameter = "mu"))
 names(coef(mod, parameter = "sigma"))
 
-plot(mod)
 
-
-# do predictions
-pred = predict(mod, newdata = grid, type = "response")
-pred = data.table(h3_index = grid$h3_index, mu = pred$mu, sigma = pred$sigma)
-
-
+# check results from stability selection (not discussed in the report)
 stab = stabsel(# FIXME
   x = mod
   , cutoff = 0.8
@@ -65,8 +63,28 @@ stab = stabsel(# FIXME
 plot(stab)
 
 
+# do predictions
+pred = predict(mod, newdata = grid, type = "response")
+pred = data.table(h3_index = grid$h3_index, mu = pred$mu, sigma = pred$sigma)
 
-# check results
+save(cl, mod, stab, pred, file = here("models", "9dkw7wyn.rda"))
 
 
-save(mod, pred, file = here("models", "9dkw7wyn.rda"))
+
+# also fit a binomial model to compare the predictive distributions 
+mod.binom = gamboost(
+  formula = frml.1$mu
+  , data = cl
+  , family = Binomial(type = "glm")
+  , control = boost_control(mstop = 1000L, nu = 0.25, trace = TRUE)
+)
+
+cv.binom = cvrisk(
+  object = mod.binom
+  , grid = 1:mstop(mod.binom)
+  , folds = cv(weights = model.weights(mod.binom), type = "subsampling", B = 25L, strata = cl$strata)
+)
+
+mod.binom[mstop(cv.binom)]
+
+save(cl, mod, mod.binom, file = here("models", "fg5kpezg.rda"))
