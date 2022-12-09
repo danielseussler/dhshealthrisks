5ef537ce64128d8ff8ecce7e351602f433a8894f
danielseussler <daniel.seussler@gmail.com>
2022-12-09 16:45:56 +0100

    Update analysis.R

--- src/analysis/madagascar/analysis.R
+++ src/analysis/madagascar/analysis.R
@@ -3,31 +3,32 @@
 #
 #
 
-library(here)
 library(mboost)
 library(surveyCV)
 
 set.seed(seed = 1L)
 options("mc.cores" = detectCores())
 
-load(file = here::here("data", "cleaned", "madagascar", "surveydata.rda"))
-source(file = here::here("source", "madagascar", "formula.R"))
-source(file = here::here("utils", "DeselectBoost.R"))
+load(file = here::here("data", "processed", "madagascar", "surveydata.rda"))
+source(file = here::here("src", "analysis", "madagascar", "formula.R"))
+source(file = here::here("src", "utils", "DeselectBoost.R"))
+
 
 # estimate model
 mod = gamboost(
-  formula = frml.1
+  formula = frml.2
   , data = sv
   , family = Binomial(type = "adaboost", link = "logit")
   , control = boost_control(mstop = 1000L, nu = 0.1)
 )
-
 
 # resample model to obtain
 cv = cvrisk(
   object = mod
   , folds = 1L * as.matrix(1L == replicate(25L, folds.svy(sv, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
 )
+
+mstop(mod) = mstop(cv)
 
 plot(cv, main = "25-fold survey stratified cluster sampling")
 
@@ -50,8 +51,9 @@ stab.2 = stabsel(
   , PFER = 5L
   , sampling.type = "SS"
   , assumption = "unimodal"
-  , folds = 1L * as.matrix(1L == replicate(50, folds.svy(sv, nfolds = 2, strataID = "strata", clusterID = "cluster")))
+  , folds = 1L * as.matrix(1L == replicate(50L, folds.svy(sv, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
 )
+
 
 par(mfrow = c(1,2))
 plot(stab, type = "maxsel", np = 20)
@@ -59,6 +61,7 @@ plot(stab.2, type = "maxsel", np = 20)
 
 plot(stab, type = "path")
 plot(stab.2, type = "path")
+
 
 # refit with stability selected?
 
@@ -78,6 +81,8 @@ plot(mod.new$risk(), type = "l")
 mod[mstop(cv)]
 
 modsp = DeselectBoost(mod, fam = Binomial(), data = sv)
+
+length(names(coef(mod)))
 length(names(coef(modsp)))
 
 
