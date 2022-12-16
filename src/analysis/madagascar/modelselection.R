e1865b539183d9073c140668fd59131f0b8d2139
danielseussler <daniel.seussler@gmail.com>
2022-12-16 19:39:34 +0100

    Update of modelselection.R

--- src/analysis/madagascar/modelselection.R
+++ src/analysis/madagascar/modelselection.R
@@ -1,7 +1,7 @@
-# comparison of different model definitions
+# comparison 
+# 
+# 
 # test on non-linearties and interactions with boosted trees
-#
-#
 
 library(here)
 library(data.table)
@@ -19,15 +19,16 @@ load(file = here("data", "processed", "madagascar", "surveydata.rda"))
 source(file = here("src", "analysis", "madagascar", "formula.R"))
 source(file = here("src", "utils", "func_purr_progress.R"))
 
-ITER = 50L # number of repeats
-ITER.ss = 25L # number of subsampling folds in inner folds
+FOLDS = 10L # number of repeats
+NUMSS = 25L # number of cluster subsampling folds in inner folds
 
 
 # define outer folds on which models are evaluated
 # ideally one should not drop one strata from the analysis, but to conduct the
 # resampling at least three clusters within a strata are required
+
 sv = subset(sv, strata != 2L) |> droplevels()
-sv.folds = replicate(ITER, folds.svy(sv, nfold = 3L, strataID = "strata", clusterID = "cluster"))
+sv.folds = replicate(FOLDS, folds.svy(sv, nfold = 3L, strataID = "strata", clusterID = "cluster"))
 
 
 # that is the main model
@@ -39,54 +40,13 @@ sim.1 = function(.model = NULL, .formula = NULL, .iter = NULL) {
   mod = gamboost(
     formula = .formula
     , data = training
-    , family = Binomial()
-    , control = boost_control(mstop = 1000L, nu = 0.1, trace = FALSE)
+    , family = Binomial(type = "glm", link = "logit")
+    , control = boost_control(mstop = 2000L, nu = 0.25, trace = FALSE)
   )
 
   cv = cvrisk(
     object = mod
-    , grid = 1:mstop(mod)
-    , folds = 1L * as.matrix(1L == replicate(ITER.ss, folds.svy(training, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
-  )
-
-  mod[mstop(cv)]
-
-  dt = data.table(
-    model = .model
-    , iter = .iter
-    , mstop = mstop(cv)
-    , actual = testing$hazx
-    , pred = predict(object = mod, newdata = testing, type = "response")
-  )
-
-  return(dt)
-}
-
-res.1 = map_with_progress(1:ITER, ~ sim.1("A", frml.1, .))
-res.2 = map_with_progress(1:ITER, ~ sim.1("B", frml.2, .)) # with gendered effects
-res.3 = map_with_progress(1:ITER, ~ sim.1("C", frml.3, .)) # with urban effects
-
-
-# tree boosting to assess if higher order interactions are present in the data
-
-sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
-
-  training = sv[which(sv.folds[, .iter] != 3L), ]
-  testing = sv[which(sv.folds[, .iter] == 3L), ]
-
-  mod = blackboost(
-    formula = .formula
-    , data = training
-    , family = Binomial()
-    , control = boost_control(mstop = 300L, nu = 0.01, trace = FALSE)
-    , tree_controls = partykit::ctree_control(maxdepth = 3L, saveinfo = FALSE)
-  )
-
-  cv = cvrisk(
-    object = mod
-    , grid = 1:mstop(mod)
-    # , folds = cv(model.weights(mod), type  = "bootstrap", strata = training$strata)
-    , folds = 1L * as.matrix(1L == replicate(ITER.ss, folds.svy(training, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
+    , folds = 1L * as.matrix(1L == replicate(NUMSS, folds.svy(training, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
   )
 
   mod[mstop(cv)]
@@ -102,7 +62,45 @@ sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
   return(dt)
 }
 
-res.tree = map_with_progress(1:ITER, ~ sim.tree("D", frml.tree, .))
+res.1 = map_with_progress(1:FOLDS, ~ sim.1("A", frml.1, .))
+res.2 = map_with_progress(1:FOLDS, ~ sim.1("B", frml.2, .)) # with gendered effects
+res.3 = map_with_progress(1:FOLDS, ~ sim.1("C", frml.3, .)) # with urban effects
+
+
+# tree boosting to assess if higher order interactions are present in the data
+
+sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
+
+  training = sv[which(sv.folds[, .iter] != 3L), ]
+  testing = sv[which(sv.folds[, .iter] == 3L), ]
+
+  mod = blackboost(
+    formula = .formula
+    , data = training
+    , family = Binomial(type = "glm", link = "logit")
+    , control = boost_control(mstop = 500L, nu = 0.01, trace = FALSE)
+    , tree_controls = partykit::ctree_control(maxdepth = 4L, saveinfo = FALSE)
+  )
+
+  cv = cvrisk(
+    object = mod
+    , folds = 1L * as.matrix(1L == replicate(NUMSS, folds.svy(training, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
+  )
+
+  mod[mstop(cv)]
+
+  dt = data.table(
+    model = .model
+    , iter = .iter
+    , mstop = mstop(cv)
+    , actual = testing$hazx
+    , pred = c(predict(object = mod, newdata = testing, type = "response"))
+  )
+
+  return(dt)
+}
+
+res.tree = map_with_progress(1:FOLDS, ~ sim.tree("D", frml.tree, .))
 
 
 # combine and save all predictions
