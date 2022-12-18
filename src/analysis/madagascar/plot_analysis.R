6cfe97e45ad4e595e07ffa4d5671e0a4e590af3b
danielseussler <daniel.seussler@gmail.com>
2022-12-18 12:28:59 +0100

    Update plot_analysis.R

--- src/analysis/madagascar/plot_analysis.R
+++ src/analysis/madagascar/plot_analysis.R
@@ -18,14 +18,14 @@ theme_update(
   , plot.background = element_rect(fill = "transparent", colour = NA)
 )
 
+
 # load shapes for gmrf effect plot
-shp = readRDS(file = here("data", "processed", "madagascar", "dhsboundaries.rds"))
-
 # load custom plot functions
-source(file = here("src", "utils", "func_plot_partial_effects.R"))
-
-# load fitted models
+# load fitted models and data
+shp = readRDS(file = here("data", "processed", "madagascar", "dhsboundaries.rds"))
 load(file = here("models", "h9h7v919.rda"))
+load(file = here("data", "processed", "madagascar", "surveydata.rda"))
+source(file = here("src", "utils", "func_plot_partial_effects.R"))
 
 
 # plot comparison of cv results
@@ -39,14 +39,14 @@ dev.off()
 # plot variable importance before and after Deselection of learners that attributet low to the 
 # complete risk reduction 
 plot(varimp(mod), type = "blearner", nbars = 20L)
-plot(varimp(mod.ds.atr), type = "blearner", nbars = 20L)
+plot(varimp(mod.deselect), type = "blearner", nbars = 20L)
 
 plts = vector(mode = "list")
 
 plts$A = ggplot(data.frame(varimp(mod)), aes(variable, reduction, fill = blearner)) + 
   geom_bar(stat = "identity") + coord_flip() 
 
-plts$B = ggplot(data.frame(varimp(mod.ds.atr)), aes(variable, reduction, fill = blearner)) + 
+plts$B = ggplot(data.frame(varimp(mod.deselect)), aes(variable, reduction, fill = blearner)) + 
   geom_bar(stat = "identity") + coord_flip() 
 
 patch.plts = wrap_plots(plts, ncol = 1L) +  plot_annotation(tag_levels = "A")
@@ -55,19 +55,19 @@ ggsave(plot = patch.plts, filename = "fig_mdg_variableimportance.png", path = he
 
 
 # plot main effects
-names(coef(mod.ds.atr))
+names(coef(mod.deselect))
 
 plts = vector(mode = "list")
-plts$A = plt_numeric(.mod = mod.ds.atr, .data = sv, .var = "cage", .rugged = FALSE, .xlab = "cage")
-plts$B = plt_numeric(.mod = mod.ds.atr, .data = sv, .var = "medu", .rugged = FALSE, .xlab = "medu")
-patch.plts = wrap_plots(plts, ncol = 2) +  plot_annotation(tag_levels = "A")
+plts$A = plt_numeric(.mod = mod.deselect, .data = sv, .var = "cage", .rugged = FALSE, .xlab = "cage")
+plts$B = plt_numeric(.mod = mod.deselect, .data = sv, .var = "medu", .rugged = FALSE, .xlab = "medu")
+patch.plts = wrap_plots(plts, ncol = 2L) +  plot_annotation(tag_levels = "A")
 
 ggsave(plot = patch.plts, filename = "fig_mdg_main_effects_smooth.png", path = here::here("results", "figures"), dpi = 600L, scale = 1.3, width = 200L, height = 100L, units = "mm", device = png, bg = "transparent")
 
 
 # plot spatial effect
 theme_set(theme_void())
-plt.spatial = plt_gmrf(.mod = mod.ds.atr, .data = sv, .var = "dhsregion", .shp = shp, .limscol = c(-0.8, 0.8))
+plt.spatial = plt_gmrf(.mod = mod.deselect, .data = sv, .var = "dhsregion", .shp = shp, .limscol = c(-0.8, 0.8))
 
 ggsave(plot = plt.spatial, filename = "fig_mdg_main_effects_spatial.png", path = here::here("results", "figures"), dpi = 600L, scale = 1.3, width = 200L, height = 100L, units = "mm", device = png, bg = "transparent")
 
