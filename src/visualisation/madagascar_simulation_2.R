c16f129a5f5e2703754ab789378de19cb26fa0c6
danielseussler <daniel.seussler@gmail.com>
2022-12-23 17:09:08 +0100

    Update

--- src/visualisation/madagascar_simulation_2.R
+++ src/visualisation/madagascar_simulation_2.R
@@ -1,4 +1,4 @@
-#
+# figures
 #
 #
 #
@@ -8,28 +8,39 @@ library(Metrics)
 library(data.table)
 library(ggplot2)
 
+# here I compare the estimated oob loss with the observed oob loss for sampled stopping times
+# 
+
 load(file = here("models", "nybm9n81.rda"))
-
-# results = results[checkat %in% c(500, 750, 1000, 1250)]
-results = results[checkat == 1000L]
 results[, risk_diff := true_risk - cv_risk]
+results[, risk_diff_ratio := (true_risk - cv_risk) / true_risk]
+
+results[, as.list(summary(risk_diff)), by = .(type)]
+results[, as.list(sd(risk_diff)), by = .(type)]
+
+results[, name := type]
+results[type == "k-fold stratified", name := "k-fold\nsurvey stratified"]
+results[type == "bootstrap stratified", name := "bootstrap\nsurvey stratified"]
+results[type == "subsampling survey cluster", name := "subsampling\ncluster + survey strat."]
 
 
-ggplot(data = results, mapping = aes(x = type, y = risk_diff)) + 
+plt = ggplot(data = results, mapping = aes(x = name, y = risk_diff_ratio)) + 
   geom_line(aes(group = iter), color = "grey") + 
   geom_abline(intercept = 0, slope = 0, color = "black") + 
-  # geom_line(aes(y = true_risk), color = "red") + 
   geom_boxplot(alpha = 0.5) + 
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  labs(x = "", y = expression((italic(R)- hat(italic(R))) / italic(R))) +
   facet_grid(~ holdout, scales = "free") +
-  labs(x = "", y = "")
-  
+  theme_bw()
 
-ggplot(data = results, mapping = aes(x = type, y = mstop)) + 
-  geom_line(aes(group = iter), color = "grey") + 
-  geom_abline(intercept = 0, slope = 0, color = "black") + 
-  # geom_line(aes(y = true_risk), color = "red") + 
-  geom_boxplot(alpha = 0.5) + 
-  facet_grid(~ holdout, scales = "free") +
-  labs(x = "", y = "")
-
-ggsave(plot = plt, filename = "fig_mdg_simulation_2.png", path = here("results", "figures"), scale = 1.2, dpi = 600, width = 200, height = 100, units = "mm", device = png)
+ggsave(
+  plot = plt
+  , filename = "fig_mdg_simulation_2.png"
+  , path = here("results", "figures")
+  , scale = 1.4
+  , dpi = 600
+  , width = 200
+  , height = 80
+  , units = "mm"
+  , device = png
+)
