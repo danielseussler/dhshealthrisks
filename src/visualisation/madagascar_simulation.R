0f58a7169f620341000a9dd508de7fab892fb500
danielseussler <daniel.seussler@gmail.com>
2022-12-23 17:09:14 +0100

    Update

--- src/visualisation/madagascar_simulation.R
+++ src/visualisation/madagascar_simulation.R
@@ -9,7 +9,6 @@ library(ggplot2)
 library(viridis)
 library(Metrics)
 
-theme_set(theme_bw())
 load(file = here("models", "88qlclkf.rda"))
 
 results[, predc := as.integer(pred >= 0.5)]
@@ -31,41 +30,38 @@ metr = results[,
 
 summary(metr)
 
-# k-fold fails to converge for some, in total these are only a selected few and therefore ignored
+# k-fold fails to stop for some iterations, in total these are only a selected few and therefore ignored
 metr[mstop == 2000 & method == "kfold\n10-folds 1-rep"]
 
 # results model complexity generalization error (true)
 tmp = melt(metr, id.vars = c("holdout", "method", "cvstrat"), measure.vars = c("ncoef", "mstop", "loss", "ce", "auc", "rec", "pre"))
-plt.1 = ggplot(tmp, aes(x = method, y = value, fill = cvstrat)) +
+tmp = tmp[method %in% c("bootstrap\n25-folds 1-rep", "kfold\n10-folds 1-rep", "subsampling\n25-folds 1-rep", "subsampling cluster\n25-folds 1-rep")]
+
+plt = ggplot(tmp, aes(x = method, y = value, fill = cvstrat)) +
   geom_boxplot(outlier.alpha = 0.3, alpha = 0.6) +
   scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
-  scale_fill_manual(values = viridis(n = 2, alpha = 0.9, begin = 0.3, end = 0.7), name = "Stratification") +
+  # scale_fill_manual(values = viridis(n = 2, alpha = 0.9, begin = 0.3, end = 0.7), name = "Stratification") +
+  scale_fill_grey(name = "Stratification") +
   facet_grid(variable ~ holdout, scales = "free") +
   labs(x = "", y = "") +
+  theme_bw() +
   theme(legend.position = "bottom")
 
-ggsave(plot = plt.1, filename = "fig_mdg_simresults.png", path = here("results", "figures"), dpi = 600, scale = 0.9, width = 210, height = 297, units = "mm", device = png)
+ggsave(plot = plt, filename = "fig_mdg_simulation.png", path = here("results", "figures"), dpi = 600, width = 200, height = 260, units = "mm", device = png)
 
 
 # check if there are changes on strata level prediction improvement
-# but likely highly depends on how outcome is associated to the 
-metrStrata = results[,
-                     .(ncoef = min(ncoef),
-                       mstop = min(mstop),
-                       loss = logLoss(actual, pred),
-                       acc = accuracy(actual, predc),
-                       ce = ce(actual, predc),
-                       auc = auc(actual, pred),
-                       brier = mse(actual, pred),
-                       rec = recall(actual, predc),
-                       pre = precision(actual, predc),
-                       f1 = f1(actual, predc)),
-                     by = list(holdout, method, cvstrat, strata, iter)]
+# the variance of the strata level estimates is reduced marginally at best
+
+metrStrata = results[
+  , .(ncoef = min(ncoef), mstop = min(mstop), loss = logLoss(actual, pred))
+  , by = list(holdout, method, cvstrat, strata, iter)
+]
 
 metrStrata = metrStrata[method == "subsampling cluster\n25-folds 1-rep" | (method == "kfold\n10-folds 1-rep" & cvstrat == "none")]
-metrStrata = metrStrata[, .(meanLoss = mean(loss), varLoss = var(loss)), by = .(holdout, method)]
+metrStrata[, .(meanLoss = mean(loss), varLoss = var(loss)), by = .(holdout, method)]
 
-ggplot(data = metrStrata, mapping = aes(x = strata, y = meanLoss, color = method)) +
-  geom_point() +
-  geom_line() + 
-  facet_grid(~ holdout, scales = "free")
+ggplot(data = metrStrata, mapping = aes(x = strata, y = loss, fill = method)) +
+  geom_boxplot() +
+  facet_grid(~holdout, scales = "free") +
+  theme_bw()
