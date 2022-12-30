a7e7adbd09389bed6bbdaae6901e7efe4681dced
danielseussler <daniel.seussler@gmail.com>
2022-12-30 11:53:21 +0100

    rename

--- src/visualisation/madagascar_simulation_cv.R
+++ src/visualisation/madagascar_simulation_cv.R
@@ -12,9 +12,12 @@ library(patchwork)
 
 # load simulation data
 # overlay boxplots of empirical distribution of predicited oob risk with empirical distr of true risk
-#
+
 
 load(file = here("models", "k30w39r3.rda"))
+# load(file = here("models", "k30w39r3severe.rda"))
+# load(file = here("models", "k30w39r3gaussian.rda"))
+# load(file = here("models", "sd0s5lew.rda"))
 results[, risk_diff := true_risk - cv_risk]
 results[, risk_diff_ratio := (true_risk - cv_risk) / true_risk]
 
@@ -27,12 +30,13 @@ results[type == "bootstrap stratified", name := "bootstrap\nsurvey stratified"]
 results[type == "subsampling survey cluster", name := "subsampling\ncluster + survey strat."]
 
 
+
 trueQuantilesA = quantile(subset(results, holdout == "A")$true_risk, probs = c(0.05, 0.95))
 plt.A = ggplot(data = subset(results, holdout == "A"), mapping = aes(x = name, y = cv_risk)) +
-  geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = trueQuantilesA[1], ymax = trueQuantilesA[2]), fill = "grey", color = NA, alpha = 0.2) +
+  # geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = trueQuantilesA[1], ymax = trueQuantilesA[2]), fill = "grey", color = NA, alpha = 0.2) +
   geom_boxplot(alpha = 0.5) +
+  geom_boxplot(aes(y = true_risk), alpha = 0.5, color = "red") +
   scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
-  scale_y_continuous(limits = c(0.615, 0.655)) +
   labs(x = "", y = expression(italic(hat(R)))) +
   facet_grid(~holdout, scales = "free") +
   theme_bw()
