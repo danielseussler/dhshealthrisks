1840ed3bffb440168f6d2ca1ac0fc9b69294e75c
danielseussler <daniel.seussler@gmail.com>
2023-01-04 09:58:42 +0100

    Update surveys_clusterlocations.R

--- src/visualisation/surveys_clusterlocations.R
+++ src/visualisation/surveys_clusterlocations.R
@@ -3,21 +3,20 @@
 #
 #
 
-library(here)
 library(ggplot2)
 library(viridis)
 library(sf)
 
 
 # data
-shp_eth = readRDS(file = here("data", "processed", "ethiopia", "dhsboundaries.rds"))
-clr_eth = readRDS(file = here("data", "raw", "rdhs", "ETGE81FL.rds")) |> st_as_sf()
+shp_eth = readRDS(file = file.path("data", "processed", "ethiopia", "dhsboundaries.rds"))
+clr_eth = readRDS(file = file.path("data", "raw", "rdhs", "ETGE81FL.rds")) |> st_as_sf()
 
-shp_mdg = readRDS(file = here("data", "processed", "madagascar", "dhsboundaries.rds"))
-clr_mdg = readRDS(file = here("data", "raw", "rdhs",  "MDGE81FL.rds")) |> st_as_sf()
+shp_mdg = readRDS(file = file.path("data", "processed", "madagascar", "dhsboundaries.rds"))
+clr_mdg = readRDS(file = file.path("data", "raw", "rdhs",  "MDGE81FL.rds")) |> st_as_sf()
 
-shp_mli = readRDS(file = here("data", "processed", "mali", "dhsboundaries.rds"))
-clr_mli = readRDS(file = here("data", "raw", "rdhs",  "MLGE81FL.rds")) |> st_as_sf() |> subset(LONGNUM != 0)
+shp_mli = readRDS(file = file.path("data", "processed", "mali", "dhsboundaries.rds"))
+clr_mli = readRDS(file = file.path("data", "raw", "rdhs",  "MLGE81FL.rds")) |> st_as_sf() |> subset(LONGNUM != 0)
 
 
 # plots
@@ -36,6 +35,6 @@ plt_mli = ggplot() +
   geom_sf(data = clr_mli, size = 0.7)
 
 
-ggsave(plot = plt_eth, filename = "fig_eth_clusterloc.png", path = here("results", "figures"), dpi = 600, width = 200, height = 200, units = "mm", device = png)
-ggsave(plot = plt_mdg, filename = "fig_mdg_clusterloc.png", path = here("results", "figures"), dpi = 600, width = 200, height = 200, units = "mm", device = png)
-ggsave(plot = plt_mli, filename = "fig_mli_clusterloc.png", path = here("results", "figures"), dpi = 600, width = 200, height = 200, units = "mm", device = png)
+ggsave(plot = plt_eth, filename = "ethiopia_clusterlocations.png", path = file.path("results", "figures"), dpi = 600, width = 200, height = 200, units = "mm", device = png)
+ggsave(plot = plt_mdg, filename = "madagascar_clusterlocations.png", path = file.path("results", "figures"), dpi = 600, width = 200, height = 200, units = "mm", device = png)
+ggsave(plot = plt_mli, filename = "mali_clusterlocations.png", path = file.path("results", "figures"), dpi = 600, width = 200, height = 200, units = "mm", device = png)
