83070f538b862b4b5bf9132621af84d3fd6a2413
danielseussler <daniel.seussler@gmail.com>
2023-01-04 10:05:08 +0100

    update formula.R

--- src/analysis/madagascar/formula.R
+++ src/analysis/madagascar/formula.R
@@ -26,11 +26,12 @@ frml.1 = hazf ~
   bols(car) +
   bols(fews) +
 
-  bols(cage) + bbs(cage, center = TRUE, df = 1, knots = 20) +
-  bols(mage) + bbs(mage, center = TRUE, df = 1, knots = 20) +
-  bols(medu) + bbs(medu, center = TRUE, df = 1, knots = 20) +
-  bols(healthaccess) + bbs(healthaccess, center = TRUE, df = 1, knots = 20) +
-  bols(cityaccess) + bbs(cityaccess, center = TRUE, df = 1, knots = 20) +
+  bols(cage) + bbs(cage, center = TRUE, df = 1L, knots = 20L) +
+  bols(mage) + bbs(mage, center = TRUE, df = 1L, knots = 20L) +
+  bols(mbmi) + bbs(mbmi, center = TRUE, df = 1L, knots = 20L) +
+  bols(medu) + bbs(medu, center = TRUE, df = 1L, knots = 20L) +
+  bols(healthaccess) + bbs(healthaccess, center = TRUE, df = 1L, knots = 20L) +
+  bols(cityaccess) + bbs(cityaccess, center = TRUE, df = 1L, knots = 20L) +
 
   bmrf(dhsregion, bnd = nb)
 
@@ -58,11 +59,12 @@ frml.2 = hazf ~
   bols(car) + bols(car, by = csex) +
   bols(fews) + bols(fews, by = csex) +
 
-  bols(cage) + bols(cage, by = csex) + bbs(cage, center = TRUE, df = 1, knots = 20) + bbs(cage, by = csex, center = TRUE, df = 1, knots = 20) +
-  bols(mage) + bols(mage, by = csex) + bbs(mage, center = TRUE, df = 1, knots = 20) +  bbs(mage, by = csex, center = TRUE, df = 1, knots = 20) +
-  bols(medu) + bols(medu, by = csex) + bbs(medu, center = TRUE, df = 1, knots = 20) + bbs(medu, by = csex, center = TRUE, df = 1, knots = 20) +
-  bols(healthaccess) + bols(healthaccess, by = csex) + bbs(healthaccess, center = TRUE, df = 1, knots = 20) + bbs(healthaccess, by = csex, center = TRUE, df = 1, knots = 20) +
-  bols(cityaccess) + bols(cityaccess, by = csex) + bbs(cityaccess, center = TRUE, df = 1, knots = 20) + bbs(cityaccess, by = csex, center = TRUE, df = 1, knots = 20) +
+  bols(cage) + bols(cage, by = csex) + bbs(cage, center = TRUE, df = 1L, knots = 20L) + bbs(cage, by = csex, center = TRUE, df = 1L, knots = 20L) +
+  bols(mage) + bols(mage, by = csex) + bbs(mage, center = TRUE, df = 1L, knots = 20L) +  bbs(mage, by = csex, center = TRUE, df = 1L, knots = 20L) +
+  bols(mbmi) + bols(mbmi, by = csex) + bbs(mbmi, center = TRUE, df = 1L, knots = 20L) + bbs(mbmi, by = csex, center = TRUE, df = 1L, knots = 20L) +
+  bols(medu) + bols(medu, by = csex) + bbs(medu, center = TRUE, df = 1L, knots = 20L) + bbs(medu, by = csex, center = TRUE, df = 1L, knots = 20L) +
+  bols(healthaccess) + bols(healthaccess, by = csex) + bbs(healthaccess, center = TRUE, df = 1L, knots = 20L) + bbs(healthaccess, by = csex, center = TRUE, df = 1L, knots = 20L) +
+  bols(cityaccess) + bols(cityaccess, by = csex) + bbs(cityaccess, center = TRUE, df = 1L, knots = 20L) + bbs(cityaccess, by = csex, center = TRUE, df = 1L, knots = 20L) +
 
   bmrf(dhsregion, bnd = nb) + bmrf(dhsregion, bnd = nb, by = csex)
 
@@ -91,50 +93,18 @@ frml.3 = hazf ~
   bols(car) + bols(car, by = urban) +
   bols(fews) +  bols(fews, by = urban) +
 
-  bols(cage) + bols(cage, by = urban) + bbs(cage, center = TRUE, df = 1, knots = 20) + bbs(cage, by = urban, center = TRUE, df = 1, knots = 20) +
-  bols(mage) + bols(mage, by = urban) + bbs(mage, center = TRUE, df = 1, knots = 20) + bbs(mage, by = urban, center = TRUE, df = 1, knots = 20) +
-  bols(medu) + bols(mage, by = urban) + bbs(medu, center = TRUE, df = 1, knots = 20) + bbs(medu, by = urban, center = TRUE, df = 1, knots = 20) +
-  bols(healthaccess) + bols(healthaccess, by = urban) + bbs(healthaccess, center = TRUE, df = 1, knots = 20) + bbs(healthaccess, by = urban, center = TRUE, df = 1, knots = 20) +
-  bols(cityaccess) + bols(cityaccess, by = urban) + bbs(cityaccess, center = TRUE, df = 1, knots = 20) + bbs(cityaccess, by = urban, center = TRUE, df = 1, knots = 20) +
+  bols(cage) + bols(cage, by = urban) + bbs(cage, center = TRUE, df = 1L, knots = 20L) + bbs(cage, by = urban, center = TRUE, df = 1L, knots = 20L) +
+  bols(mage) + bols(mage, by = urban) + bbs(mage, center = TRUE, df = 1L, knots = 20L) + bbs(mage, by = urban, center = TRUE, df = 1L, knots = 20L) +
+  bols(mbmi) + bols(mbmi, by = urban) + bbs(mbmi, center = TRUE, df = 1L, knots = 20L) + bbs(mbmi, by = urban, center = TRUE, df = 1L, knots = 20L) +
+  bols(medu) + bols(mage, by = urban) + bbs(medu, center = TRUE, df = 1L, knots = 20L) + bbs(medu, by = urban, center = TRUE, df = 1L, knots = 20L) +
+  bols(healthaccess) + bols(healthaccess, by = urban) + bbs(healthaccess, center = TRUE, df = 1L, knots = 20L) + bbs(healthaccess, by = urban, center = TRUE, df = 1L, knots = 20L) +
+  bols(cityaccess) + bols(cityaccess, by = urban) + bbs(cityaccess, center = TRUE, df = 1L, knots = 20L) + bbs(cityaccess, by = urban, center = TRUE, df = 1L, knots = 20L) +
 
   bmrf(dhsregion, bnd = nb) + bmrf(dhsregion, bnd = nb, by = urban)
-
-
-# frml.4 = hazf ~
-#   bols(denom, intercept = FALSE) +
-# 
-#   bols(urban) + bols(urban, by = csex) +
-#   bols(csex) + bols(csex, by = urban) +
-#   bols(ctwin) + bols(ctwin, by = csex) + bols(ctwin, by = urban) +
-#   bols(cbord) + bols(cbord, by = csex) + bols(cbord, by = urban) +
-#   bols(hmembers) + bols(hmembers, by = csex) + bols(hmembers, by = urban) +
-#   bols(mreligion) + bols(mreligion, by = csex) + bols(mreligion, by = urban) +
-#   bols(memployed) + bols(memployed, by = csex) + bols(memployed, by = urban) +
-#   bols(nodead) + bols(nodead, by = csex) + bols(nodead, by = urban) +
-#   bols(watersource) + bols(watersource, by = csex) + bols(watersource, by = urban) +
-# 
-#   bols(sanitation) + bols(sanitation, by = csex) + bols(sanitation, by = urban) +
-#   bols(wealth) + bols(wealth, by = csex) + bols(wealth, by = urban) +
-#   bols(electricity) + bols(electricity, by = csex) + bols(electricity, by = urban) +
-#   bols(radio) + bols(radio, by = csex) + bols(radio, by = urban) +
-#   bols(television) + bols(television, by = csex) + bols(television, by = urban) +
-#   bols(refrigerator) + bols(refrigerator, by = csex) + bols(refrigerator, by = urban) +
-#   bols(bicycle) + bols(bicycle, by = csex) + bols(bicycle, by = urban) +
-#   bols(motorcycle) + bols(motorcycle, by = csex) + bols(motorcycle, by = urban) +
-#   bols(car) + bols(car, by = csex) + bols(car, by = urban) +
-#   bols(fews) + bols(fews, by = csex) + bols(fews, by = urban) +
-# 
-#   bols(cage) + bols(cage, by = csex) + bols(cage, by = urban) + bbs(cage, center = TRUE, df = 1, knots = 20) +
-#   bols(mage) + bols(mage, by = csex) + bols(mage, by = urban) + bbs(mage, center = TRUE, df = 1, knots = 20) +
-#   bols(medu) + bols(medu, by = csex) + bols(mage, by = urban) + bbs(medu, center = TRUE, df = 1, knots = 20) +
-#   bols(healthaccess) + bols(healthaccess, by = urban) + bols(healthaccess, by = csex) + bbs(healthaccess, center = TRUE, df = 1, knots = 20) +
-#   bols(cityaccess) + bols(cityaccess, by = urban) + bols(cityaccess, by = csex) + bbs(cityaccess, center = TRUE, df = 1, knots = 20) +
-# 
-#   bmrf(dhsregion, bnd = nb)
 
 
 frml.tree = hazf ~ denom + urban + csex + ctwin + cbord + hmembers + mreligion + 
   memployed + nodead + watersource + sanitation + wealth + electricity + radio + 
   television + refrigerator + bicycle + motorcycle + car + fews + cage + mage + 
-  medu + healthaccess + cityaccess + dhsregion
+  mbmi + medu + healthaccess + cityaccess + dhsregion
  
