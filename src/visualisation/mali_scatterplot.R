d914e25b5f309348ed91a1b154f2953ed8d09449
danielseussler <daniel.seussler@gmail.com>
2023-01-04 09:58:59 +0100

    Update mali_scatterplot.R

--- src/visualisation/mali_scatterplot.R
+++ src/visualisation/mali_scatterplot.R
@@ -3,13 +3,12 @@
 #
 #
 
-library(here)
 library(ggplot2)
 library(viridis)
 
 theme_set(theme_classic())
 
-load(file = here("data", "processed", "mali", "surveydata.rda"))
+load(file = file.path("data", "processed", "mali", "surveydata.rda"))
 
 plt = ggplot(data = cl, mapping = aes(x = n, y = npos, shape = urban, color = urban)) +
   geom_point() +
@@ -20,7 +19,13 @@ plt = ggplot(data = cl, mapping = aes(x = n, y = npos, shape = urban, color = ur
   scale_color_manual(values = viridis(n = 2, alpha = 0.8, begin = 0.3, end = 0.7), name = "Type") +
   scale_y_continuous(expand = expansion(add = 1)) +
   scale_x_continuous(limits = c(0, 110), expand = expansion(add = 0)) +
-  scale_shape(name = "Type") + 
+  scale_shape(name = "Type") +
   theme(legend.position = c(.1, .9))
 
-ggsave(plot = plt, filename = "fig_mli_scatterplot.png", path = here("results", "figures"), dpi = 600, width = 200, height = 100, units = "mm", device = png)
+ggsave(
+  plot = plt
+  , filename = "mali_scatterplot.png"
+  , path = file.path("results", "figures")
+  , dpi = 600, width = 200, height = 100
+  , units = "mm", device = png
+)
