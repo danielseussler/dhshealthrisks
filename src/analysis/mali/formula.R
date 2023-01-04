725d95802cb76b128990276b806daf67d00cbaa9
danielseussler <daniel.seussler@gmail.com>
2023-01-04 10:01:46 +0100

    update

--- src/analysis/mali/formula.R
+++ src/analysis/mali/formula.R
@@ -3,36 +3,21 @@
 #
 #
 
-# dependent variable
-# y = matrix(c(cl$npos, cl$nneg), ncol = 2)
-
 frml.1 = list(
-  mu = y ~
+  mu = pos ~
     bols(intercept, intercept = FALSE) +
+    bols(urban) + bols(climate) +
 
-    bols(urban) +
-    bols(climate) +
+    bols(elev) + bbs(elev, center = TRUE, df = 1L, knots = 20L) +
+    bols(lstday) + bbs(lstday, center = TRUE, df = 1L, knots = 20L) +
 
-    bols(elev) +
-    bbs(elev, center = TRUE, df = 1L, knots = 20L) +
+    bols(lstnight) + bbs(lstnight, center = TRUE, df = 1L, knots = 20L) +
+    bols(ndvi) + bbs(ndvi, center = TRUE, df = 1L, knots = 20L) +
 
-    bols(lstday) +
-    bbs(lstday, center = TRUE, df = 1L, knots = 20L) +
+    bols(evi) + bbs(evi, center = TRUE, df = 1L, knots = 20L) +
+    bols(precip) + bbs(precip, center = TRUE, df = 1L, knots = 20L) +
 
-    bols(lstnight) +
-    bbs(lstnight, center = TRUE, df = 1L, knots = 20L) +
-
-    bols(ndvi) +
-    bbs(ndvi, center = TRUE, df = 1L, knots = 20L) +
-
-    bols(evi) +
-    bbs(evi, center = TRUE, df = 1L, knots = 20L) +
-
-    bols(precip) +
-    bbs(precip, center = TRUE, df = 1L, knots = 20L) +
-
-    bols(pop) +
-    bbs(pop, center = TRUE, df = 1L, knots = 20L) +
+    bols(log_pop) + bbs(log_pop, center = TRUE, df = 1L, knots = 20L) +
 
     # brad(lon, lat, knots = 100, df = 1L, covFun = fields::stationary.cov,
     #      args = list(Covariance = "Matern", smoothness = 1.5, theta = NULL)),
@@ -40,7 +25,7 @@ frml.1 = list(
     bols(lon) + bols(lat) + bols(lon, by = lat) +
     bspatial(lon, lat, df = 1L, center = TRUE),
 
-  sigma = y ~
+  sigma = pos ~
     bols(intercept, intercept = FALSE) +
     bols(urban) +
 
@@ -53,6 +38,6 @@ frml.1 = list(
 
 
 frml.tree = list(
-  mu = y ~ intercept + urban + climate + elev + lstday + lstnight + ndvi + evi + precip + pop + lon + lat,
-  sigma = y ~ intercept + urban + lon + lat
+  mu = pos ~ intercept + urban + climate + elev + lstday + lstnight + ndvi + evi + precip + log_pop + lon + lat,
+  sigma = pos ~ intercept + urban + lon + lat
 )
