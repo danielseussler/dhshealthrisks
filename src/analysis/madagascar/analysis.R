621b22c54003a451dc70ec6e543f6a65f411bacb
danielseussler <daniel.seussler@gmail.com>
2023-01-04 10:05:17 +0100

    update analysis.R

--- src/analysis/madagascar/analysis.R
+++ src/analysis/madagascar/analysis.R
@@ -9,32 +9,58 @@ library(surveyCV)
 set.seed(seed = 1L)
 options("mc.cores" = detectCores())
 
-load(file = here::here("data", "processed", "madagascar", "surveydata.rda"))
-source(file = here::here("src", "analysis", "madagascar", "formula.R"))
-source(file = here::here("src", "utils", "DeselectBoost.R"))
+load(file = file.path("data", "processed", "madagascar", "surveydata.rda"))
+source(file = file.path("src", "analysis", "madagascar", "formula.R"))
+source(file = file.path("src", "utils", "DeselectBoost.R"))
 
+sv$moderatelyx = with(sv, ifelse(haz < -200, 1L, 0L))
+sv$moderatelyf = factor(sv$moderatelyx, levels = c(0L, 1L), labels = c("no", "yes"))
+
+sv$severelyx = with(sv, ifelse(haz < -300, 1L, 0L))
+sv$severelyf = factor(sv$severelyx, levels = c(0L, 1L), labels = c("no", "yes"))
 
 
 # estimate main model based on the simple formula 
 # resample model to with cluster sub sampling and survey stratification to select optimal number of 
 # boosting iterations and apply early stopping
 
-fittedModel = gamboost(
-  formula = frml.1
+fitModerately = gamboost(
+  formula = update(frml.1, moderatelyf ~ .)
   , data = sv
   , family = Binomial(type = "glm", link = "logit")
-  , control = boost_control(mstop = 5000L, nu = 0.25, trace = TRUE)
+  , control = boost_control(mstop = 10000L, nu = 0.1, trace = TRUE)
 )
 
-ssResample = cvrisk(
-  object = fittedModel
-  , grid = seq(25L, 5000L, by = 10L)
-  , folds = as.matrix(1L == replicate(25L, folds.svy(sv, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
+cvModerately = cvrisk(
+  object = fitModerately
+  , grid = seq(from = 25L, to = mstop(fitModerately), by = 10L)
+  , folds = cv(weights = model.weights(fitModerately), type = "subsampling", B = 25L, prob = 0.8, strata = sv$strata)
 )
 
-plot(ssResample, main = "25-fold survey straified cluster subsampling")
+plot(cvModerately, main = "25-fold survey straified subsampling")
 
-fittedModel[mstop(ssResample)]
+fitModerately[mstop(cvModerately)]
+names(coef(fitModerately))
+
+
+# for severely stunted children only
+fitSeverely = gamboost(
+  formula = update(frml.1, severelyf ~ .)
+  , data = sv
+  , family = Binomial(type = "glm", link = "logit")
+  , control = boost_control(mstop = 10000L, nu = 0.1, trace = TRUE)
+)
+
+cvSeverely = cvrisk(
+  object = fitSeverely
+  , grid = seq(from = 25L, to = mstop(fitSeverely), by = 10L)
+  , folds = cv(weights = model.weights(fitSeverely), type = "subsampling", B = 50L, prob = 0.8, strata = sv$strata)
+)
+
+plot(cvSeverely, main = "25-fold survey straified subsampling")
+
+fitSeverely[mstop(cvSeverely)]
+names(coef(cvSeverely))
 
 
 
@@ -46,30 +72,51 @@ fittedModel[mstop(ssResample)]
 
 # note: the warning thrown is because the function searches for the response var in the data frame, 
 # and converts to numerics which throws this error converting region char, no issue with the estimation
-length(names(coef(fittedModel, which = "")))
-names(coef(fittedModel))
+length(names(coef(fitModerately, which = "")))
+names(coef(fitModerately))
+plot(varimp(fitModerately), percent = TRUE, nbars = 30, type = "blearner")
 
-plot(varimp(fittedModel), percent = TRUE, nbars = 22, type = "blearner")
-
-deselectModel = DeselectBoost(
-  object = fittedModel
+deselectModerately = DeselectBoost(
+  object = fitModerately
   , fam = Binomial(type = "glm", link = "logit")
   , data = sv
   , tau = 0.01
   , method = "attributable"
 ) 
 
-deselectModelCum = DeselectBoost(
-  object = fittedModel
+deselectModeratelyCum = DeselectBoost(
+  object = fitModerately
   , fam = Binomial(type = "glm", link = "logit")
   , data = sv
   , tau = 0.01
   , method = "cumulative"
 ) 
 
-names(coef(fittedModel))
-names(coef(deselectModel))
-names(coef(deselectModelCum))
+names(coef(fitModerately))
+names(coef(deselectModerately))
+names(coef(deselectModeratelyCum))
+
+
+
+deselectSeverely = DeselectBoost(
+  object = fitSeverely
+  , fam = Binomial(type = "glm", link = "logit")
+  , data = sv
+  , tau = 0.01
+  , method = "attributable"
+) 
+
+deselectSeverelyCum = DeselectBoost(
+  object = fitSeverely
+  , fam = Binomial(type = "glm", link = "logit")
+  , data = sv
+  , tau = 0.01
+  , method = "cumulative"
+) 
+
+names(coef(fitSeverely))
+names(coef(deselectSeverely))
+names(coef(deselectSeverelyCum))
 
 
 
@@ -80,11 +127,11 @@ names(coef(deselectModelCum))
 # assumptions on the distribution of 'noise' variables to derive bounds without the initial assumptions
 # is thereby only controlling the number of *expected number of selected variables with low selection probability*
 
-stabsel_parameters(x = fittedModel, q = 10L, PFER = 2L, sampling.type = "SS")
-stabsel_parameters(x = fittedModel, q = 10L, PFER = 4L, sampling.type = "MB")
+stabsel_parameters(x = fitModerately, q = 10L, PFER = 2L, sampling.type = "SS")
+stabsel_parameters(x = fitModerately, q = 10L, PFER = 4L, sampling.type = "MB")
 
-stabMB = stabsel(
-  x = fittedModel
+stabModeratelyMB = stabsel(
+  x = fitModerately
   , q = 10L
   , PFER = 4L
   , grid = 0:5000
@@ -92,8 +139,8 @@ stabMB = stabsel(
   , folds = 1L * as.matrix(1L == replicate(100L, folds.svy(sv, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
 )
 
-stabSS = stabsel(
-  x = fittedModel
+stabModeratelySS = stabsel(
+  x = fitModerately
   , q = 10L
   , PFER = 2L
   , grid = 0:5000
@@ -102,13 +149,15 @@ stabSS = stabsel(
   , folds = 1L * as.matrix(1L == replicate(50L, folds.svy(sv, nfolds = 2L, strataID = "strata", clusterID = "cluster")))
 )
 
-plot(stabMB, type = "maxsel", np = 20L, main = "Stability Selection MB")
-plot(stabSS, type = "maxsel", np = 20L, main = "Stability Selection SS")
+plot(stabModeratelyMB, type = "maxsel", np = 20L, main = "Stability Selection MB")
+plot(stabModeratelySS, type = "maxsel", np = 20L, main = "Stability Selection SS")
 
-plot(stabMB, type = "path")
-plot(stabSS, type = "path")
+plot(stabModeratelyMB, type = "path")
+plot(stabModeratelySS, type = "path")
+
 
 save(
-  fittedModel, ssResample, deselectModel, deselectModelCum, stabMB, stabSS
-  , file = here::here("models", "h9h7v919.rda")
+  fitModerately, fitSeverely, cvModerately, cvSeverely, deselectModerately, deselectModeratelyCum, 
+  deselectSeverely, deselectSeverelyCum, stabModeratelyMB, stabModeratelySS
+  , file = file.path("models", "h9h7v919.rda")
 )
