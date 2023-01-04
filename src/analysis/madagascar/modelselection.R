1946adfca257ec13d59b676d0c23e4271200a43a
danielseussler <daniel.seussler@gmail.com>
2023-01-04 10:05:49 +0100

    Update modelselection.R

--- src/analysis/madagascar/modelselection.R
+++ src/analysis/madagascar/modelselection.R
@@ -1,9 +1,8 @@
-# comparison 
-# 
-# 
+# model selection 
+# by nested cross-validation
 # test on non-linearties and interactions with boosted trees
+# 
 
-library(here)
 library(data.table)
 library(mboost)
 library(rsample)
@@ -15,12 +14,12 @@ library(Metrics)
 set.seed(seed = 1L)
 options("mc.cores" = detectCores())
 
-load(file = here("data", "processed", "madagascar", "surveydata.rda"))
-source(file = here("src", "analysis", "madagascar", "formula.R"))
-source(file = here("src", "utils", "func_purrr_progress.R"))
+load(file = file.path("data", "processed", "madagascar", "surveydata.rda"))
+source(file = file.path("src", "analysis", "madagascar", "formula.R"))
+source(file = file.path("src", "utils", "func_purrr_progress.R"))
 
-FOLDS = 10L # number of repeats
-NUMSS = 25L # number of cluster subsampling folds in inner folds
+numFolds = 10L # number of repeats
+numSS = 25L # number of subsampling folds in inner folds
 
 
 # define outer folds on which models are evaluated
@@ -28,11 +27,11 @@ NUMSS = 25L # number of cluster subsampling folds in inner folds
 # resampling at least three clusters within a strata are required
 
 sv = subset(sv, strata != 2L) |> droplevels()
-sv.folds = replicate(FOLDS, folds.svy(sv, nfold = 3L, strataID = "strata", clusterID = "cluster"))
+sv.folds = replicate(numFolds, folds.svy(sv, nfold = 3L, strataID = "strata", clusterID = "cluster"))
 
 
 # that is the main model
-sim.1 = function(.model = NULL, .formula = NULL, .iter = NULL) {
+sim.fml = function(.model = NULL, .formula = NULL, .iter = NULL) {
 
   training = sv[which(sv.folds[, .iter] != 3L), ]
   testing = sv[which(sv.folds[, .iter] == 3L), ]
@@ -47,25 +46,23 @@ sim.1 = function(.model = NULL, .formula = NULL, .iter = NULL) {
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(weights = model.weights(mod), type = "subsampling", strata = training$strata)
+    , folds = cv(weights = model.weights(mod), type = "subsampling", B = numSS, strata = training$strata, prob = 0.8)
   )
-
-  mod[mstop(cv)]
 
   dt = data.table(
     model = .model
     , iter = .iter
     , mstop = mstop(cv)
     , actual = testing$hazx
-    , pred = c(predict(object = mod, newdata = testing, type = "response"))
+    , pred = c(predict(object = mod[mstop(cv)], newdata = testing, type = "response"))
   )
 
   return(dt)
 }
 
-res.1 = map_with_progress(1:FOLDS, ~ sim.1("A", frml.1, .)) # base model
-res.2 = map_with_progress(1:FOLDS, ~ sim.1("B", frml.2, .)) # with gendered effects
-res.3 = map_with_progress(1:FOLDS, ~ sim.1("C", frml.3, .)) # with urban effects
+res.1 = map_with_progress(1:numFolds, ~ sim.fml("A", frml.1, .)) # base model
+res.2 = map_with_progress(1:numFolds, ~ sim.fml("B", frml.2, .)) # with gendered effects
+res.3 = map_with_progress(1:numFolds, ~ sim.fml("C", frml.3, .)) # with urban effects
 
 
 # tree boosting to assess if higher order interactions are present in the data
@@ -86,23 +83,21 @@ sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(weights = model.weights(mod), type = "kfold", strata = training$strata)
+    , folds = cv(weights = model.weights(mod), type = "subsampling", B = numSS, strata = training$strata, prob = 0.8)
   )
   
-  mod[mstop(cv)]
-
   dt = data.table(
     model = .model
     , iter = .iter
     , mstop = mstop(cv)
     , actual = testing$hazx
-    , pred = c(predict(object = mod, newdata = testing, type = "response"))
+    , pred = c(predict(object = mod[mstop(cv)], newdata = testing, type = "response"))
   )
 
   return(dt)
 }
 
-res.tree = map_with_progress(1:FOLDS, ~ sim.tree("D", frml.tree, .))
+res.tree = map_with_progress(1:numFolds, ~ sim.tree("D", frml.tree, .))
 
 
 # combine and save all predictions
@@ -110,4 +105,4 @@ res = list(res.1, res.2, res.3, res.tree) |>
   lapply(rbindlist) |>
   rbindlist()
 
-save(res, file = here("models", "lvcw0a2q.rda"))
+save(res, file = file.path("models", "lvcw0a2q.rda"))
