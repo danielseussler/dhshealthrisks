4c27f57935236f247051b282802d32f7e4f4f044
danielseussler <daniel.seussler@gmail.com>
2023-01-04 10:02:15 +0100

    add admin designation to grid

--- src/analysis/mali/geodata.R
+++ src/analysis/mali/geodata.R
@@ -3,7 +3,6 @@
 #
 #
 
-library(here)
 library(terra)
 library(sf)
 library(h3)
@@ -11,12 +10,25 @@ library(data.table)
 
 
 # load data and create regular grid of locations
-shp = readRDS(file = here("data", "processed", "mali", "dhsboundaries.rds"))
-indices = polyfill(st_union(shp), res = 7L) # average area of hexagon is 5.161293360
+dhs_shp = readRDS(file = file.path("data", "processed", "mali", "dhsboundaries.rds"))
+gadm_shp = readRDS(file = file.path("data", "processed", "mali", "regions.rds"))[]
+indices = polyfill(st_union(dhs_shp), res = 7L) # average area of hexagon is 5.161293360
 coords = h3_to_geo(indices)
-grid = data.table(h3_index = indices, lon = coords[, 2L] , lat = coords[, 1L])
 
-cloc = readRDS(file = here("data", "raw", "rdhs", "MLGE81FL.rds"))
+# extract admin 1 region for each
+coords_sf = st_as_sf(data.frame(coords), coords = c("lng", "lat"), crs = st_crs(dhs_shp))
+
+admin1_intersect = as.integer(st_intersects(coords_sf$geometry, dhs_shp))
+admin2_intersect = st_intersects(coords_sf$geometry, gadm_shp$admin2)
+admin2_intersect[as.integer(lapply(admin2_intersect, length)) > 1] = NA # remove multiple matches, are only 2
+admin2_intersect = as.integer(admin2_intersect)
+
+grid = data.table(h3_index = indices, lon = coords[, 2L] , lat = coords[, 1L])
+grid[, admin1 := ifelse(is.na(admin1_intersect), NA, dhs_shp$DHSREGEN[admin1_intersect])]
+grid[, admin2 := ifelse(is.na(admin2_intersect), NA, gadm_shp$admin2$NAME_2[admin2_intersect])]
+
+# load grid clusters
+cloc = readRDS(file = file.path("data", "raw", "rdhs", "MLGE81FL.rds"))
 cloc = cloc |> st_as_sf() |> st_drop_geometry() |> as.data.table()
 cloc = dplyr::select(cloc, cluster = DHSCLUST, urban = URBAN_RURA, lon = LONGNUM, lat = LATNUM)
 cloc = dplyr::mutate(cloc, urban = factor(urban, levels = c("R", "U"), labels = c("rural", "urban")))
@@ -24,40 +36,39 @@ cloc = subset(cloc, lon != 0)
 
 
 # elevation
-elev = geodata::elevation_30s(country = "Mali", path = here("data", "raw", "geodata"))
+elev = geodata::elevation_30s(country = "Mali", path = file.path("data", "raw", "geodata"))
 
 plot(elev)
 
 # population von GHSL
-pop = rast(x = here("data", "raw", "GHS_POP_E2020_GLOBE_R2022A_54009_1000_V1_0", "GHS_POP_E2020_GLOBE_R2022A_54009_1000_V1_0.tif"))
-pop = crop(x = pop, y = st_transform(shp, crs = "+proj=moll"))
+pop = rast(x = file.path("data", "raw", "GHS_POP_E2020_GLOBE_R2022A_54009_1000_V1_0", "GHS_POP_E2020_GLOBE_R2022A_54009_1000_V1_0.tif"))
+pop = crop(x = pop, y = st_transform(dhs_shp, crs = "+proj=moll"))
 pop = project(x = pop, y = "epsg:4326", method = "bilinear")
 
-plot(log(mask(x = pop, mask = shp)))
+plot(log(mask(x = pop, mask = dhs_shp)))
 
 
 # global climate zones
-climzone = rast(x = here("data", "raw", "Beck_KG_V1", "Beck_KG_V1_present_0p083.tif"))
-climzone = crop(x = climzone, y = shp)
+climzone = rast(x = file.path("data", "raw", "Beck_KG_V1", "Beck_KG_V1_present_0p083.tif"))
+climzone = crop(x = climzone, y = dhs_shp)
 
 # climzone = as.factor(climzone)
 # val = data.frame(ID = c(3, 4, 6), label = c("Tropical, savannah", "Arid, desert, hot", "Arid, steppe, hot"))
 # climzone = categories(climzone, value = val, index = 2)
 
-plot(mask(x = climzone, mask = shp))
+plot(mask(x = climzone, mask = dhs_shp))
 
-# urban
-urban = rast(x = here("data/raw/GHS_SMOD_E2020_GLOBE_R2022A_54009_1000_V1_0/GHS_SMOD_E2020_GLOBE_R2022A_54009_1000_V1_0.tif"))
-urban = crop(x = urban, y = st_transform(shp, crs = "+proj=moll"))
+# urbanicity
+urban = rast(x = file.path("data/raw/GHS_SMOD_E2020_GLOBE_R2022A_54009_1000_V1_0/GHS_SMOD_E2020_GLOBE_R2022A_54009_1000_V1_0.tif"))
+urban = crop(x = urban, y = st_transform(dhs_shp, crs = "+proj=moll"))
 urban = project(x = urban, y = "epsg:4326", method = "near")
 plot(urban)
 
 # load google earth engine raster files
-fpath = list.files(path = here("data", "raw", "earthengine"), full.names = TRUE, pattern = "Mali")
+fpath = list.files(path = file.path("data", "raw", "earthengine"), full.names = TRUE, pattern = "Mali")
 rsd = rast(x = fpath)
 
-plot(mask(x = rsd, mask = shp))
-
+plot(mask(x = rsd, mask = dhs_shp))
 
 # extract data for the grid
 grid$elev = extract(elev, grid[, .(lon, lat)], method = "bilinear", ID = FALSE)
@@ -76,7 +87,6 @@ grid = cbind(grid, tmp)
 
 grid = grid[complete.cases(grid)]
 
-
 # extract data for the cluster locations
 cloc$pop = extract(pop, cloc[, .(lon, lat)], method = "bilinear", ID = FALSE)
 cloc$elev = extract(elev, cloc[, .(lon, lat)], method = "bilinear", ID = FALSE)
@@ -87,7 +97,6 @@ cloc$climate = factor(cloc$climate, levels = c(3L, 4L, 6L), labels = c("Tropical
 tmp = extract(x = rsd, cloc[, .(lon, lat)], method = "bilinear", ID = FALSE)
 colnames(tmp) = c("evi", "lstday", "lstnight", "ndvi", "precip", "water_mask")
 cloc = cbind(cloc, tmp)
-
 
 # alternative construct urban / rural indicator, I finally use the SMOD GHSL
 # because of the (slighlty) higher share of regions classified as urban
@@ -100,5 +109,5 @@ cloc = cbind(cloc, tmp)
 # grid$urban = factor(grid$urban, levels = c(FALSE, TRUE), labels = c("rural", "urban"))
 # sum(grid$pop[grid$urban == "urban"]) / sum(grid$pop) # check if urban proportion was correct: Yes
 
-
-save(cloc, grid, file = here("data", "processed", "mali", "geodata.rda"))
+# save data
+save(cloc, grid, file = file.path("data", "processed", "mali", "geodata.rda"))
