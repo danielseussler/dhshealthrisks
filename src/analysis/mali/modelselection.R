5bc640cc1f51855a7b2522972c9244d24d9b70aa
danielseussler <daniel.seussler@gmail.com>
2023-01-04 10:04:48 +0100

    update modelselection.R

--- src/analysis/mali/modelselection.R
+++ src/analysis/mali/modelselection.R
@@ -3,7 +3,6 @@
 #
 #
 
-library(here)
 library(data.table)
 library(mboost)
 library(gamboostLSS)
@@ -15,49 +14,59 @@ library(Metrics)
 set.seed(seed = 1L)
 options("mc.cores" = detectCores())
 
-load(file = here("data", "processed", "mali", "surveydata.rda"))
-load(file = here("data", "processed", "mali", "geodata.rda"))
-source(file = here("src", "analysis", "mali", "formula.R"))
+load(file = file.path("data", "processed", "mali", "surveydata.rda"))
+load(file = file.path("data", "processed", "mali", "geodata.rda"))
+source(file = file.path("src", "analysis", "mali", "formula.R"))
+
 cl = dplyr::left_join(cl, cloc)
+cl = dplyr::mutate(cl, log_pop = log(pop + 1.0))
 
 
 # define outer folds on which models are evaluated
 # the issue with repeated kfolds is that sometimes to few obs in small strata are observed
 # therefore the analysis is limited to one repeat
 rsp = vfold_cv(data = cl, v = 10L, repeats = 1L, strata = strata)
-res.1 = res.2 = res.3 = data.table()
+res = data.table()
 
 
 # the complete script could be implemented way better, but mboost/gamboostLSS seems to have a problem
 # finding the boundary matrix in the correct environment, this is therefore implemented over loops
 
-# main model 
+# main model
 # distributional regression with beta-binomial distributed outcome
 for (i in 1:nrow(rsp)) {
 
   trainingData = analysis(rsp$splits[[i]])
   testData = assessment(rsp$splits[[i]])
-  y = matrix(c(trainingData$npos, trainingData$nneg), ncol = 2L)
+  pos = matrix(data = c(trainingData$npos, trainingData$nneg), ncol = 2L)
 
   mod = gamboostLSS(
     formula = frml.1
     , data = trainingData
     , families = as.families("BB")
     , method = "noncyclic"
-    , control = boost_control(mstop = 1000L, nu = 0.25, trace = FALSE)
+    , control = boost_control(mstop = 2000L, nu = 0.1, trace = TRUE)
   )
-  
+
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(model.weights(mod), type = "subsampling", B = 25L, strata = trainingData$strata)
+    , folds = cv(model.weights(mod), type = "subsampling", B = 25L, strata = trainingData$strata, prob = 0.8)
   )
-  
-  mod[mstop(cv)]
 
-  pred = predict(object = mod, newdata = testData, type = "response")
+  pred = predict(object = mod[mstop(cv)], newdata = testData, type = "response")
 
-  res.1 = rbindlist(list(res.1, data.table(model = "A", id = i, mstop = mstop(cv), k = testData$npos, n = testData$n, mu = pred$mu, sigma = pred$sigma[, 1L])))
+  temp_result = data.table(
+    model = "A"
+    , id = i
+    , mstop = mstop(cv)
+    , k = testData$npos
+    , n = testData$n
+    , mu = c(pred$mu)
+    , sigma = c(pred$sigma)
+  )
+
+  res = rbindlist(list(res, temp_result))
 }
 
 
@@ -69,26 +78,32 @@ for (i in 1:nrow(rsp)) {
 
   trainingData = analysis(rsp$splits[[i]])
   testData = assessment(rsp$splits[[i]])
-  y = matrix(c(trainingData$npos, trainingData$nneg), ncol = 2L)
+  pos = matrix(data = c(trainingData$npos, trainingData$nneg), ncol = 2L)
 
   mod = gamboost(
     formula = frml.1$mu
     , data = trainingData
     , family = Binomial(type = "glm")
-    , control = boost_control(mstop = 1000L, nu = 0.1, trace = FALSE)
+    , control = boost_control(mstop = 2000L, nu = 0.1, trace = FALSE)
   )
 
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(model.weights(mod), type = "subsampling", B = 25L, strata = trainingData$strata)
+    , folds = cv(model.weights(mod), type = "subsampling", B = 25L, strata = trainingData$strata, prob = 0.8)
   )
 
-  mod[mstop(cv)]
+  temp_result = data.table(
+    model = "B"
+    , id = i
+    , mstop = mstop(cv)
+    , k = testData$npos
+    , n = testData$n
+    , mu = c(predict(object = mod[mstop(cv)], newdata = testData, type = "response"))
+    , sigma = 0
+  )
 
-  pred = predict(object = mod, newdata = testData, type = "response")
-
-  res.2 = rbindlist(list(res.2, data.table(model = "B", id = i, mstop = mstop(cv), k = testData$npos, n = testData$n, mu = pred[, 1L], sigma = NA)))
+  res = rbindlist(list(res, temp_result))
 }
 
 
@@ -100,7 +115,7 @@ for (i in 1:nrow(rsp)) {
 
   trainingData = analysis(rsp$splits[[i]])
   testData = assessment(rsp$splits[[i]])
-  y = matrix(c(trainingData$npos, trainingData$nneg), ncol = 2L)
+  pos = matrix(data = c(trainingData$npos, trainingData$nneg), ncol = 2L)
 
   mod = blackboostLSS(
     formula = frml.tree
@@ -114,18 +129,24 @@ for (i in 1:nrow(rsp)) {
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(model.weights(mod), type = "subsampling", B = 25L, strata = trainingData$strata)
+    , folds = cv(model.weights(mod), type = "subsampling", B = 25L, strata = trainingData$strata, prob = 0.8)
   )
 
-  mod[mstop(cv)]
+  pred = predict(object = mod[mstop(cv)], newdata = testData, type = "response")
 
-  pred = predict(object = mod, newdata = testData, type = "response")
+  temp_result = data.table(
+    model = "C"
+    , id = i
+    , mstop = mstop(cv)
+    , k = testData$npos
+    , n = testData$n
+    , mu = c(pred$mu)
+    , sigma = c(pred$sigma)
+  )
 
-  res.3 = rbindlist(list(res.3, data.table(model = "C", id = i, mstop = mstop(cv), k = testData$npos, n = testData$n, mu = pred$mu, sigma = pred$sigma[, 1L])))
+  res = rbindlist(list(res, temp_result))
 }
 
 
-
-# save
-res = rbindlist(list(res.1, res.2, res.3))
-save(res, file = here("models", "4r35hoz4.rda"))
+# save results
+save(res, file = file.path("models", "4r35hoz4.rda"))
