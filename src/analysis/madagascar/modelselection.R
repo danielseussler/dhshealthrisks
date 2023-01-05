4bd6004d4733fd0ca5e916f6408e42ab3a5c56e7
danielseussler <daniel.seussler@gmail.com>
2023-01-05 11:17:09 +0100

    Update modelselection.R

--- src/analysis/madagascar/modelselection.R
+++ src/analysis/madagascar/modelselection.R
@@ -19,8 +19,6 @@ source(file = file.path("src", "analysis", "madagascar", "formula.R"))
 source(file = file.path("src", "utils", "func_purrr_progress.R"))
 
 numFolds = 10L # number of repeats
-numSS = 25L # number of subsampling folds in inner folds
-
 
 # define outer folds on which models are evaluated
 # ideally one should not drop one strata from the analysis, but to conduct the
@@ -33,28 +31,28 @@ sv.folds = replicate(numFolds, folds.svy(sv, nfold = 3L, strataID = "strata", cl
 # that is the main model
 sim.fml = function(.model = NULL, .formula = NULL, .iter = NULL) {
 
-  training = sv[which(sv.folds[, .iter] != 3L), ]
-  testing = sv[which(sv.folds[, .iter] == 3L), ]
+  training_data = sv[which(sv.folds[, .iter] != 3L), ]
+  test_data = sv[which(sv.folds[, .iter] == 3L), ]
 
   mod = gamboost(
     formula = .formula
-    , data = training
+    , data = training_data
     , family = Binomial(type = "glm", link = "logit")
-    , control = boost_control(mstop = 2000L, nu = 0.25, trace = FALSE)
+    , control = boost_control(mstop = 5000L, nu = 0.1, trace = FALSE)
   )
 
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(weights = model.weights(mod), type = "subsampling", B = numSS, strata = training$strata, prob = 0.8)
+    , folds = cv(weights = model.weights(mod), type = "kfold", B = 10, strata = training_data$strata)
   )
 
   dt = data.table(
     model = .model
     , iter = .iter
     , mstop = mstop(cv)
-    , actual = testing$hazx
-    , pred = c(predict(object = mod[mstop(cv)], newdata = testing, type = "response"))
+    , actual = test_data$hazx
+    , pred = c(predict(object = mod[mstop(cv)], newdata = test_data, type = "response"))
   )
 
   return(dt)
@@ -66,15 +64,14 @@ res_3 = map_with_progress(1:numFolds, ~ sim.fml("C", frml.3, .)) # with urban ef
 
 
 # tree boosting to assess if higher order interactions are present in the data
-
 sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
 
-  training = sv[which(sv.folds[, .iter] != 3L), ]
-  testing = sv[which(sv.folds[, .iter] == 3L), ]
+  training_data = sv[which(sv.folds[, .iter] != 3L), ]
+  test_data = sv[which(sv.folds[, .iter] == 3L), ]
 
   mod = blackboost(
     formula = .formula
-    , data = training
+    , data = training_data
     , family = Binomial(type = "glm", link = "logit")
     , control = boost_control(mstop = 1000L, nu = 0.1, trace = FALSE)
     , tree_controls = partykit::ctree_control(maxdepth = 4L, saveinfo = FALSE)
@@ -83,15 +80,15 @@ sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
   cv = cvrisk(
     object = mod
     , grid = seq(from = 25L, to = mstop(mod), by = 10L)
-    , folds = cv(weights = model.weights(mod), type = "subsampling", B = numSS, strata = training$strata, prob = 0.8)
+    , folds = cv(weights = model.weights(mod), type = "kfold", B = 10, strata = training_data$strata)
   )
 
   dt = data.table(
     model = .model
     , iter = .iter
     , mstop = mstop(cv)
-    , actual = testing$hazx
-    , pred = c(predict(object = mod[mstop(cv)], newdata = testing, type = "response"))
+    , actual = test_data$hazx
+    , pred = c(predict(object = mod[mstop(cv)], newdata = test_data, type = "response"))
   )
 
   return(dt)
@@ -100,9 +97,43 @@ sim.tree = function(.model = NULL, .formula = NULL, .iter = NULL) {
 res_4 = map_with_progress(1:numFolds, ~ sim.tree("D", frml.tree, .))
 
 
+# include a simple boosted linear model
+sim.glm = function(.model = NULL, .iter = NULL) {
+
+  training_data = sv[which(sv.folds[, .iter] != 3L), ]
+  test_data = sv[which(sv.folds[, .iter] == 3L), ]
+
+  mod = mboost(
+    formula = update(frml.tree, . ~ . - denom)
+    , data = training_data
+    , family = Binomial(type = "glm", link = "logit")
+    , control = boost_control(mstop = 5000L, nu = 0.1, trace = FALSE)
+    , baselearner = "bols"
+  )
+
+  cv = cvrisk(
+    object = mod
+    , grid = seq(from = 25L, to = mstop(mod), by = 10L)
+    , folds = cv(weights = model.weights(mod), type = "kfold", B = 10, strata = training_data$strata)
+  )
+
+  dt = data.table(
+    model = .model
+    , iter = .iter
+    , mstop = mstop(cv)
+    , actual = test_data$hazx
+    , pred = c(predict(object = mod[mstop(cv)], newdata = test_data, type = "response"))
+  )
+
+  return(dt)
+}
+
+res_5 = map_with_progress(1:numFolds, ~ sim.glm("E", .))
+
+
 # combine and save all predictions
-re = list(res_1, res_2, res_3, res_4) |>
+results = list(res_1, res_2, res_3, res_4, res_5) |>
   lapply(rbindlist) |>
   rbindlist()
 
-save(res, file = file.path("models", "lvcw0a2q.rda"))
+save(results, file = file.path("models", "lvcw0a2q.rda"))
