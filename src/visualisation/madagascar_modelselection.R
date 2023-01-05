f7c1b39c048e3993577f52a66e4fcb111d71fa1b
danielseussler <daniel.seussler@gmail.com>
2023-01-05 11:16:41 +0100

    Update madagascar_modelselection.R

--- src/visualisation/madagascar_modelselection.R
+++ src/visualisation/madagascar_modelselection.R
@@ -1,4 +1,4 @@
-# check model selection
+# plot model selection
 #
 #
 #
@@ -9,30 +9,37 @@ library(ggplot2)
 
 load(file = file.path("models", "lvcw0a2q.rda"))
 
-res[, predc := as.integer(pred >= 0.5)]
-res[model == "A", name := "Base model"]
-res[model == "B", name := "Base model\n+ gender interaction"]
-res[model == "C", name := "Base model\n+ urbanicity interaction"]
-res[model == "D", name := "Boosted with\nregression trees"]
+results[, class := as.integer(pred >= 0.5)]
+results[model == "A", name := "Base model"]
+results[model == "B", name := "Base model\n+ gender interaction"]
+results[model == "C", name := "Base model\n+ urbanicity interaction"]
+results[model == "D", name := "Boosted w/\nregression trees"]
+results[model == "E", name := "Linear model"]
 
-metricsSummary = res[,
-                     .(mstop = min(mstop)
-                       , loss = logLoss(actual, pred)
-                       , acc = accuracy(actual, predc)
-                       , ce = ce(actual, predc)
-                       , auc = auc(actual, pred)
-                       , brier = mse(actual, pred)
-                       , rec = recall(actual, predc)
-                       , pre = precision(actual, predc)),
-                     by = list(name, iter)]
+metrics_summary = results[
+  , .(loss = logLoss(actual, pred)
+      , acc = accuracy(actual, class)
+      , ce = ce(actual, class)
+      , auc = auc(actual, pred)
+      , brier = mse(actual, pred)
+      , rec = recall(actual, class)
+      , pre = precision(actual, class))
+  , by = list(name, iter)
+]
 
-metricsSummary
+metrics_summary
 
-plt = ggplot(data = metricsSummary, mapping = aes(x = name, y = loss)) +
+ggplot(data = metrics_summary, mapping = aes(x = name, y = auc)) +
   geom_line(mapping = aes(group = iter), color = "grey") +
   geom_boxplot(alpha = 0.5, width = 0.5) +
   labs(x = "", y = expression(hat(R))) +
-  theme_bw()
+  theme_minimal()
+
+plt = ggplot(data = metrics_summary, mapping = aes(x = name, y = loss)) +
+  geom_line(mapping = aes(group = iter), color = "grey") +
+  geom_boxplot(alpha = 0.5, width = 0.5) +
+  labs(x = "", y = expression(hat(R))) +
+  theme_minimal()
 
 ggsave(
   plot = plt
